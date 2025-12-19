1f647e713e63cab5fd63c649a3cbf34d6494a417
Tariq <tariqali87b@gmail.com>
2025-12-19 15:01:16 +0000

    changes to seed

--- seeds/social_network.sql
+++ seeds/social_network.sql
@@ -1,30 +1,30 @@
 DROP TABLE IF EXISTS posts;
-DROP SEQUENCE IF EXISTS posts_id_seq;
+
 DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS users_id_seq;
+
 
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email text
-    username text
+    email TEXT,
+    username TEXT
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    title text,
-    content text,
-    views int
+    title TEXT,
+    content TEXT,
+    views INT,
     -- The foreign key name is always {other_table_singular}_id
-    user_id int,
-    constraint fk_user foreign key(user_id)
-    references users(id)
-    on delete cascade
+    user_id INT,
+    CONSTRAINT fk_user FOREIGN KEY(user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
 );
 
 INSERT INTO users (email, username) VALUES ('tariqali87b@gmail.com', 'tariq');
 INSERT INTO users (email, username) VALUES ('yusuf@gmail.com', 'yusuf');
 
-INSERT INTO posts (title, contents, views, user_id) VALUES ('title one', 'content one', 1, 1);
-INSERT INTO posts (title, contents, views, user_id) VALUES ('title two', 'content two', 1, 2);
+INSERT INTO posts (title, content, views, user_id) VALUES ('title one', 'content one', 1, 1);
+INSERT INTO posts (title, content, views, user_id) VALUES ('title two', 'content two', 1, 2);
 
